7efcdbd0b43f53d9389a31390edb25afd01a86ed
Sankalan Parajuli <sankalan.13@gmail.com>
2019-09-19 22:09:32 +0530

    added docker-compose for selenium grid and parallel testing

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,18 +5,23 @@ exports.config = {
     framework: 'jasmine',
     capabilities: {
       browserName: 'chrome',
+      shardTestFiles: true,
+      maxInstances: 5,
       chromeOptions: {
         args: [ "--headless", "--disable-gpu" ]
       }
     },
     specs: [
-      './src/specs/demo.spec.js'
+      './src/specs/*.spec.js'
     ],
-    directConnect: true,
+    // directConnect: true,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
   
     // You could set no globals to true to avoid jQuery '$' and protractor '$'
     // collisions on the global namespace.
     noGlobals: false,
+    getPageTimeout: 180000,
+    allScriptsTimeout: 180000,
 
     onPrepare() { 
       // let browser = globals.browser;
@@ -26,6 +31,7 @@ exports.config = {
      }).getJasmine2Reporter());
       browser.driver.manage().window().maximize();
       browser.driver.manage().window().setPosition(0, 0);
+      browser.manage().timeouts().implicitlyWait(5000);
     }, 
     onComplete() { 
       browser.close();
